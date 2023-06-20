ee531584ef7acf68494db25946f2140024f5d7b9
Fiery518 <mantejtuli@gmail.com>
2023-06-20 07:26:55 +0530

    The Basics are made svelte, tailwind, vite, daisy

---
+++ School_bi/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <button class="px-10 py-3 bg-red-500">Tailwind Button</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+  </div>
+  
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style global lang = "postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
