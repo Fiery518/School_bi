2a00597ca9df9fd7c878c183cf022de4a15f40c8
Fiery518 <mantejtuli@gmail.com>
2023-06-21 07:10:38 +0530

    nothing much

--- School_bi/src/App.svelte
+++ School_bi/src/App.svelte
@@ -5,28 +5,28 @@
 </script>
 
 <main>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  </div> -->
+  <h1>Library EZ</h1>
 
   <div class="card">
     <button class="px-10 py-3 bg-red-500">Tailwind Button</button>
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-primary">Primary daisy button</button>
+    <button class="btn btn-secondary">Secondary daisy button</button>
   </div>
   
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a href="https://github.com/Fiery518/School_bi/blob/main/README.md" target="_blank" rel="noreferrer">School_bi read about it</a>, Lessss... goooooo
   </p>
 
   <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+    Will create this to be something great
   </p>
 </main>
 
